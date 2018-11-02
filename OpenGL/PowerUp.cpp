8ee71a9a690b633e976b6f5c8c6c34e5607e13cb
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2018-11-02 10:14:37 +0800

    fixed the powerup not chaning

--- OpenGL/PowerUp.cpp
+++ OpenGL/PowerUp.cpp
@@ -64,6 +64,20 @@ bool PowerUp::CheckCollisionOnplayer(b2Body* _player) {
 		isactive = false;
 		lifetimer = 0;
 		m_physicsBody->SetTransform(b2Vec2(rand() % 18 + 1, rand() % 13 + 1), m_physicsBody->GetAngle());
+
+		type = rand() % 3 + 1;
+
+		if (type == 1) {
+			m_mesh = std::make_unique<Mesh>("Resources/Textures/PowerupBomb.png", m_shader);
+		}
+		else if (type == 2) {
+			m_mesh = std::make_unique<Mesh>("Resources/Textures/PowerupMultiball.png", m_shader);
+		}
+		else if (type == 3) {
+			m_mesh = std::make_unique<Mesh>("Resources/Textures/PowerupSpeed.png", m_shader);
+		}
+
+
 		return true;
 	}
 
